2cce76774f5b96019c214e745ffd0274cb72cd65
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-08-27 13:19:33 +0200

    gradle shadow plugin

--- duels/build.gradle.kts
+++ duels/build.gradle.kts
@@ -1,3 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("com.gradleup.shadow") version "8.3.0"
+}
+
 version = "1.0.0-SNAPSHOT"
 
 repositories {
@@ -13,4 +20,17 @@ repositories {
 dependencies {
     implementation(project(":lib"))
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+}
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("HyroDuels-${version}.jar")
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
+}
+
+artifacts {
+    archives(tasks.named<ShadowJar>("shadowJar"))
 }
--- survival/build.gradle.kts
+++ survival/build.gradle.kts
@@ -1,3 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+        plugins {
+            kotlin("jvm") version "2.0.0"
+            id("com.gradleup.shadow") version "8.3.0"
+        }
+
 version = "1.0.0-SNAPSHOT"
 
 repositories {
@@ -14,4 +21,17 @@ dependencies {
     implementation(project(":lib"))
     implementation("com.jeff-media:custom-block-data:2.2.2")
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+}
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("HyroSurvival-${version}.jar")
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
+}
+
+artifacts {
+    archives(tasks.named<ShadowJar>("shadowJar"))
 }
