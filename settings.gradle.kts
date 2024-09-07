d1d2865c0035716fe970033943abc19bd8e42a70
Marcos Corrochano Arroyo <83825951+Hyro32@users.noreply.github.com>
2024-09-07 13:01:33 +0200

    new smp module 0.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "sparkmc"
 include("proxy", "lib")
+include("smp")
---
+++ smp/src/main/java/one/hyro/spark/smp/SparkSmp.java
@@ -0,0 +1,15 @@
+package one.hyro.spark.smp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SparkSmp extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("SparkSmp has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("SparkSmp has been disabled!");
+    }
+}
